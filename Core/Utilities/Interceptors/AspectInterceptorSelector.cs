9c1f6f8ee4d17d1c65322c7a7a4678d0f0313aaa
denizduman1 <ce.denizduman2@gmail.com>
2022-09-19 21:36:10 +0300

    v6

---
+++ Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -0,0 +1,21 @@
+﻿using Castle.DynamicProxy;
+using System.Reflection;
+
+namespace Core.Utilities.Interceptors
+{
+    public class AspectInterceptorSelector : IInterceptorSelector
+    {
+        public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+        {
+            var classAtributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>(true).ToList();
+            var methodAttributes = type?.GetMethod(method.Name)?.GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
+
+            if (methodAttributes != null)
+            {
+                classAtributes.AddRange(methodAttributes);
+            }
+
+            return classAtributes.OrderBy(x => x.Priority).ToArray();
+        }
+    }
+}
---
+++ Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Interceptors
+{
+    //Autofac extras dynamic proxy 6.0.1
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
+    {
+        public int Priority { get; set; }
+        public virtual void Intercept(IInvocation invocation)
+        {
+            
+        }
+    }
+}
